777ec780f9ae17d086785999d01a7c454417cfa7
Senthil Palani <senthil@aveoninfotech.com>
2020-07-25 15:59:28 +0530

    color changes

--- components/orders/OrderItemComponent.tsx
+++ components/orders/OrderItemComponent.tsx
@@ -4,6 +4,7 @@ import {OrderItemModel} from '@app/models';
 import {format} from 'date-fns';
 import Icon from 'react-native-vector-icons/Entypo';
 import {Colors} from '@app/themes';
+import Dash from 'react-native-dash';
 
 type OrderProps = {
   item: OrderItemModel;
@@ -43,7 +44,7 @@ export const OrderItem: FunctionComponent<OrderProps> = ({item}) => {
               {format(item.orderedDate, 'do MMM,yy')}
             </Text>
           </View>
-          <View>
+          <View style={{marginRight: 20}}>
             <Text style={styles.title}>TOTAL</Text>
             <Text style={styles.titleContent}>₹{item.total}</Text>
           </View>
@@ -51,32 +52,48 @@ export const OrderItem: FunctionComponent<OrderProps> = ({item}) => {
       </View>
       {item.orderStatus === 'Open' && (
         <>
-          <View style={styles.timeLine}>
-            <View>
-              <Text style={styles.circleActive}></Text>
+          <View style={styles.row}>
+            <Dash
+              style={styles.timeLineDot}
+              dashColor={Colors.primaryColor}
+              dashGap={2}
+              dashLength={4}
+              dashThickness={1}
+            />
+            <View style={styles.timeLine}>
+              <View>
+                <Text style={styles.circleActive}></Text>
+                <Text style={styles.circleInnerActive}></Text>
+              </View>
+              <View style={{marginLeft: 20}}>
+                <Text style={styles.circle}></Text>
+              </View>
+              <View style={{marginRight: 6}}>
+                <Text style={{...styles.circle}}></Text>
+              </View>
             </View>
-            <View>
-              <Text style={styles.circle}></Text>
-            </View>
-            <View>
-              <Text style={styles.circle}></Text>
-            </View>
+            <Dash
+              style={styles.timeLineDot}
+              dashColor={Colors.primaryColor}
+              dashGap={2}
+              dashLength={4}
+              dashThickness={1}
+            />
           </View>
           <View style={styles.timeLineContainer}>
-            <View>
+            <View style={{marginLeft: 20}}>
               <Text
                 style={{
                   ...styles.timeLineLabel,
                   ...styles.timeLineLabelActive,
-                  paddingLeft: 15,
                 }}>
                 Placed
               </Text>
             </View>
-            <View>
+            <View style={{marginLeft: 20}}>
               <Text style={styles.timeLineLabel}>Out for delivery</Text>
             </View>
-            <View>
+            <View style={{marginRight: 10}}>
               <Text style={styles.timeLineLabel}>Delivered</Text>
             </View>
           </View>
@@ -86,54 +103,58 @@ export const OrderItem: FunctionComponent<OrderProps> = ({item}) => {
         <View style={styles.innerItem}>
           <View style={styles.divider}></View>
           <View>
-            <Text style={styles.title}>Shipping Details</Text>
+            <Text style={styles.itemTitle}>Shipping Details</Text>
             <View style={styles.card}>
-              <View style={styles.container}>
-                <Text style={{...styles.titleContent, ...styles.name}}>
-                  Jestin James
+              <View style={styles.innerContainerLeft}>
+                <View style={styles.container}>
+                  <Text style={{...styles.titleContent, ...styles.name}}>
+                    Jestin James
+                  </Text>
+                  <Text style={styles.addressType}>Home</Text>
+                </View>
+                <Text style={styles.label}>B2-204, Snn Raj Greenbay</Text>
+                <Text style={styles.label}>
+                  1st Main Road, Stadium road, Ernakulam, Kerala
                 </Text>
-                <Text style={styles.addressType}>Home</Text>
+                <Text style={styles.label}>Mob: +91-8945632545</Text>
               </View>
-              <Text style={styles.label}>B2-204, Snn Raj Greenbay</Text>
-              <Text style={styles.label}>
-                1st Main Road, Stadium road, Ernakulam, Kerala
-              </Text>
-              <Text style={styles.label}>Mob: +91-8945632545</Text>
             </View>
           </View>
 
           <View>
-            <Text style={styles.title}>Order Details</Text>
+            <Text style={styles.itemTitle}>Order Details</Text>
             <View style={styles.card}>
-              <View style={styles.container}>
+              <View style={{...styles.container, ...styles.innerContainer}}>
                 <Text style={styles.label}>Total Items</Text>
                 <Text style={styles.label}>2</Text>
               </View>
-              <View style={styles.container}>
+              <View style={{...styles.container, ...styles.innerContainer}}>
                 <Text style={styles.label}>Sub Total</Text>
                 <Text style={styles.label}>₹806</Text>
               </View>
-              <View style={styles.container}>
+              <View style={{...styles.container, ...styles.innerContainer}}>
                 <Text style={styles.label}>Tax Collected</Text>
                 <Text style={styles.label}>₹20</Text>
               </View>
-              <View style={styles.container}>
+              <View style={{...styles.container, ...styles.innerContainer}}>
                 <Text style={styles.label}>Discount</Text>
                 <Text style={styles.label}>₹20</Text>
               </View>
               <View style={styles.divider}></View>
-              <View style={styles.container}>
+              <View style={{...styles.container, ...styles.innerContainer}}>
                 <Text style={styles.biggerTitleContent}>Grand Total</Text>
                 <Text style={styles.biggerTitleContent}>₹786</Text>
               </View>
             </View>
           </View>
           {item.orderStatus === 'Open' ? (
-            <Button
-              onPress={() => {}}
-              title="Cancel Order"
-              color={Colors.primaryColor}
-            />
+            <View style={{marginVertical: 5}}>
+              <Button
+                onPress={() => {}}
+                title="Cancel Order"
+                color={Colors.primaryColor}
+              />
+            </View>
           ) : (
             <View style={{...styles.card, ...styles.help}}>
               <Icon name="phone" size={20} color={Colors.primaryColor} />
@@ -155,7 +176,7 @@ export const OrderItem: FunctionComponent<OrderProps> = ({item}) => {
             <Text style={styles.subTitle}>Normally Deleiverd in 30 mins</Text>
           </View>
           <View style={{alignSelf: 'center'}}>
-            <Text style={styles.title}>4.5km</Text>
+            <Text style={{...styles.title, marginRight: 5}}>4.5km</Text>
           </View>
         </View>
       </View>
@@ -168,23 +189,27 @@ const styles = StyleSheet.create({
     padding: 10,
     marginVertical: 8,
     marginHorizontal: 16,
-    borderRadius: 5,
-    shadowColor: Colors.darkColor,
-    shadowOffset: {width: 0, height: 2},
+    borderRadius: 6,
+    shadowColor: Colors.shadowColor,
+    shadowOffset: {width:0, height: 2},
     shadowOpacity: 0.8,
-    shadowRadius: 5,
-    elevation: 1,
+    shadowRadius: 6,
+    elevation: 0.8,
   },
   innerItem: {},
   card: {
     padding: 10,
     marginVertical: 5,
-    borderRadius: 5,
-    shadowColor: Colors.darkColor,
+    borderRadius: 6,
+    shadowColor: Colors.shadowColor,
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.8,
-    shadowRadius: 5,
-    elevation: 1,
+    shadowRadius: 6,
+    elevation: 0.5,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
   },
   container: {
     display: 'flex',
@@ -193,6 +218,10 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   title: {
+    color: Colors.lightColor,
+    fontSize: 13,
+  },
+  itemTitle: {
     color: Colors.lightColor,
     fontSize: 13,
   },
@@ -218,9 +247,10 @@ const styles = StyleSheet.create({
   },
   addressType: {
     fontWeight: 'bold',
+    fontSize: 10,
     backgroundColor: '#FFEFDB',
     color: Colors.primaryColor,
-    borderWidth: 2,
+    borderWidth: 1,
     borderColor: Colors.primaryColor,
     paddingLeft: 10,
     paddingRight: 5,
@@ -231,14 +261,20 @@ const styles = StyleSheet.create({
   timeLineContainer: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     flexGrow: 1,
     marginBottom: 8,
+  },
+  timeLineDot: {
+    marginTop: 16,
+    marginBottom: 7,
+    width: 30,
+    height: 1,
   },
   timeLine: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     borderTopColor: Colors.primaryColor,
     borderTopWidth: 1,
     flexGrow: 1,
@@ -254,24 +290,38 @@ const styles = StyleSheet.create({
     color: Colors.darkColor,
   },
   circleActive: {
-    width: 12,
-    height: 12,
-    backgroundColor: Colors.primaryColor,
-    borderRadius: 15,
+    width: 6,
+    height: 6,
+    backgroundColor: Colors.whiteColor,
+    borderRadius: 12,
     position: 'absolute',
-    top: -7,
+    top: -6,
     borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: Colors.primaryColor,
+    textAlign: 'center',
+    padding: 4,
+  },
+  circleInnerActive: {
+    width: 6,
+    height: 6,
+    backgroundColor: Colors.primaryColor,
+    borderRadius: 8,
+    borderWidth: 1,
+    position: 'absolute',
+    top: -4,
+    left: 2,
     borderStyle: 'solid',
     borderColor: Colors.primaryColor,
     textAlign: 'center',
   },
   circle: {
-    width: 10,
-    height: 10,
+    width: 7,
+    height: 7,
     backgroundColor: Colors.whiteColor,
     borderRadius: 15,
     position: 'absolute',
-    top: -6,
+    top: -4,
     borderWidth: 1,
     borderStyle: 'solid',
     borderColor: Colors.primaryColor,
@@ -297,5 +347,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginLeft: 20,
+  },
+  innerContainer: {
+    marginLeft: 10,
+    marginRight: 30,
+  },
+  innerContainerLeft: {
+    marginLeft: 10,
   },
 });
--- themes/colors.ts
+++ themes/colors.ts
@@ -2,6 +2,7 @@ export const Colors = {
     primaryColor: "#E4860B",
     lightColor: "#54565C",
     darkColor: "#000000",
+    shadowColor: "rgba(30,36,39,0.3)",
     whiteColor: "#FFFFFF",
     dividerColor: "#EEEEEE",
     iconColor: "#6C6E73",
